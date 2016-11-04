c61be9ce0d0a46d9fe5db6bfc565966531a071fb
Kane Baccigalupi <kane.baccigalupi@gsa.gov>
2016-11-03 20:20:43 -0700

    changes signature of FileWriter.write and also the way the filename is generated

--- test/file-writer-test.js
+++ test/file-writer-test.js
@@ -1,54 +1,42 @@
 'use strict';
 
-const rimraf      = require('rimraf');
+const crypto      = require('crypto');
 const assert      = require('assert');
+const rimraf      = require('rimraf');
 const FileWriter  = require('../lib/file-writer');
 
 describe('FileWriter', () => {
-  let basePath = __dirname + '/fixtures/accessibility';
-  let writer = new FileWriter(basePath);
+  const basePath = __dirname + '/fixtures/accessibility';
+  const results  = require('./fixtures/data/with-errors.json');
+  const pageData = {
+    path: 'my-great-page',
+    size: 'mobile',
+    other: 'stuff'
+  };
+  const writer = new FileWriter(basePath);
+
+  let filename;
 
   beforeEach((done) => {
+    filename = crypto
+      .createHash('md5')
+      .update('stuff--my-great-page--mobile')
+      .digest('hex') + '.json';
     rimraf(basePath, () => { done(); });
   });
 
   it('deduces the file path based on the data passed in', () => {
-    let data = {
-      path: 'my-great-page',
-      size: 'mobile',
-      results: []
-    };
-    assert.equal(writer.filePath(data), basePath + '/my-great-page^^mobile.json');
+    assert.equal(writer.filePath(pageData), basePath + '/' + filename);
   });
 
-  it('escapes slashes in the path when determining the file path', () => {
-    let data = {
-      path: 'about/my-great-page',
-      size: 'mobile',
-      results: []
-    };
-    assert.equal(writer.filePath(data), basePath + '/about__my-great-page^^mobile.json');
-  });
-
-  it('leaves off the size information from the file path when not in data', () => {
-    let data = {
-      path: 'my-great-page',
-      results: []
-    };
-    assert.equal(writer.filePath(data), basePath + '/my-great-page.json');
-  });
-
-  it('writes data verbatim to the file, even if the directory does not exist', (done) => {
-    let data = {
-      path: 'about/my-great-page',
-      size: 'mobile',
-      results: []
-    };
-
-    writer.write(data, (err) => {
+  it('combines page data with results, and writes to file even if the directory does not exist', (done) => {
+    writer.write(pageData, results, (err) => {
       if (err) { return done(err); }
-      let savedData = require(writer.filePath(data));
-      assert.deepEqual(savedData, data);
+      let savedData = require(writer.filePath(pageData));
+      assert.deepEqual(savedData.results, results);
+      assert.deepEqual(savedData.path, pageData.path);
+      assert.deepEqual(savedData.size, pageData.size);
+      assert.deepEqual(savedData.other, pageData.other);
       done();
     });
   });
