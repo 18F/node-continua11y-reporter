37ed3611a91375b0b9d93a9c728dfe5df4556950
Kane Baccigalupi <kane.baccigalupi@gsa.gov>
2016-11-03 20:21:51 -0700

    Changes Results class to only be concerned with sorting indidents by type

--- test/results-test.js
+++ test/results-test.js
@@ -4,72 +4,35 @@ const Results = require('../lib/results');
 const assert  = require('assert');
 
 describe('Results', () => {
-  let data, results;
-  const noErrors = require('./fixtures/data/no-errors.json');
-  const withErrors = require('./fixtures/data/with-errors.json');
+  let results;
+  const data = require('./fixtures/data/with-errors.json');
 
-  describe('when there are errors', () => {
-    beforeEach(() => {
-      data = withErrors;
-      results = new Results(data);
-    });
-
-    it('assertNoErrors raises an assertion error', () => {
-      assert.throws(() => { results.assertNoErrors(); });
-    });
+  beforeEach(() => {
+    results = new Results(data);
   });
 
-  describe('when there are no errors but there are warnings', () => {
-    beforeEach(() => {
-      data = noErrors;
-      results = new Results(data);
-    });
-
-    it('assertNoErrors raises an assertion error', () => {
-      assert.doesNotThrow(() => { results.assertNoErrors(); }, assert.AssertionError);
-    });
+  it('returns a collection of errors', () => {
+    let collection = results.errors();
+    assert.equal(collection.length, 2);
+    assert(collection.every((incident) => { return incident.type === 'error'; }));
   });
 
-  describe('assertions by threshold', () => {
-    beforeEach(() => {
-      data = withErrors;
-      results = new Results(data);
-    });
+  it('returns a collection of warnings', () => {
+    let collection = results.warnings();
+    assert.equal(collection.length, 8);
+    assert(collection.every((incident) => { return incident.type === 'warning'; }));
+  });
 
-    it('assertErrorsLessThan raises an error if the number of errors is greater than the threshold', () => {
-      assert.throws(() => { results.assertErrorsLessThan(1); });
-    });
+  it('returns a collection of notices', () => {
+    let collection = results.notices();
+    assert.equal(collection.length, 107);
+    assert(collection.every((incident) => { return incident.type === 'notice'; }));
+  });
 
-    it('assertErrorsLessThan raises an error if the number errors is the same as the threshold', () => {
-      assert.throws(() => { results.assertErrorsLessThan(2); });
-    });
-
-    it('assertErrorsLessThan passes if the number of errors is greater than the threshold', () => {
-      assert.doesNotThrow(() => { results.assertErrorsLessThan(20); }, assert.AssertionError);
-    });
-
-    it('assertWarningsLessThan raises an error if the number of errors is greater than the threshold', () => {
-      assert.throws(() => { results.assertWarningsLessThan(1); });
-    });
-
-    it('assertWarningsLessThan raises an error if the number errors is the same as the threshold', () => {
-      assert.throws(() => { results.assertWarningsLessThan(8); });
-    });
-
-    it('assertWarningsLessThan passes if the number of errors is greater than the threshold', () => {
-      assert.doesNotThrow(() => { results.assertWarningsLessThan(20); }, assert.AssertionError);
-    });
-
-    it('assertNoticesLessThan raises an error if the number of errors is greater than the threshold', () => {
-      assert.throws(() => { results.assertNoticesLessThan(1); });
-    });
-
-    it('assertNoticesLessThan raises an error if the number errors is the same as the threshold', () => {
-      assert.throws(() => { results.assertNoticesLessThan(107); });
-    });
-
-    it('assertNoticesLessThan passes if the number of errors is greater than the threshold', () => {
-      assert.doesNotThrow(() => { results.assertNoticesLessThan(110); }, assert.AssertionError);
-    });
+  it('returns a object to json stringify', () => {
+    let asJSON = results.asJSON();
+    assert.deepEqual(results.errors(), asJSON.errors);
+    assert.deepEqual(results.warnings(), asJSON.warnings);
+    assert.deepEqual(results.notices(), asJSON.notices);
   });
 });
